cf00f8ef97223fa77a679f5682c06fdd57b3778f
finn-rm <finn.rawles@canonical.com>
2024-12-16 13:53:13 +0000

    Fixing search URL

--- scraper/config.py
+++ scraper/config.py
@@ -14,13 +14,12 @@ LOCATIONS = [
 MIN_PRICE_CLP = 0
 MAX_PRICE_CLP = 1200000
 MIN_BEDROOMS = 2
-ONLY_USED = True
 
 # Base URL template
 def get_url_for_location(location, offset=0):
     """Get URL for location with offset"""
     base_url = 'https://www.portalinmobiliario.com/arriendo/departamento'
-    return f'{base_url}/{location}/_Desde_{offset + 1}_NoIndex_True'
+    return f'{base_url}/{location}/_Desde_{offset + 1}_PriceRange_{MIN_PRICE_CLP}CLP-{MAX_PRICE_CLP}CLP_BEDROOMS_{MIN_BEDROOMS}-*_NoIndex_True'
 
 # Example URL Format
 # https://www.portalinmobiliario.com/arriendo/departamento/propiedades-usadas/providencia-metropolitana/_Desde_0_PriceRange_0CLP-1200000CLP_BEDROOMS_2-*_NoIndex_True
